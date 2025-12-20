d577437537a156fd612cea2b33aea20b46636ac9
mkaratarakis <mixkarat@gmail.com>
2025-12-20 11:50:48 +0100

    etc

--- Mathlib/NumberTheory/Transcendental/GelfondSchneider/setup.lean
+++ Mathlib/NumberTheory/Transcendental/GelfondSchneider/setup.lean
@@ -8,16 +8,40 @@ module
 public import Mathlib.Analysis.Complex.Basic
 public import Mathlib.NumberTheory.NumberField.Basic
 public import Mathlib.NumberTheory.NumberField.House
-public import Mathlib.NumberTheory.NumberField.InfinitePlace.Embeddings
+public import Mathlib.NumberTheory.NumberField.InfinitePlace.Embedding
+public import Mathlib.Tactic
+
 
 /-!
-These are some auxiliary lemmata covering the algebraic part of the proof of the
-Gelfond–Schneider theorem.
+# Hilbert's Seventh Problem (Gelfond–Schneider Theorem)
+The goal of this file is to formalize a proof of the **Gelfond–Schneider Theorem**, which solves
+Hilbert’s Seventh Problem: namely, that for algebraic numbers `α ≠ 0, 1` and irrational algebraic
+`β`, the number `α ^ β` is transcendental.
+
+## Main results
+* `gelfondSchneider`: If `α` and `β` are algebraic, `α ≠ 0`, `α ≠ 1`, and `β` is irrational, then
+  `α ^ β` is transcendental.
+
+## Implementation details
+We follow the proof in Keng’s *Introduction to Number Theory*, Chapter 17, Section 5, p.488 - 493.
+
+The structure of the proof is as follows:
+
+* The argument proceeds by **contradiction**. The core of the proof is
+  an **auxiliary function lemma**, where we construct a nonzero integer linear
+  combination of exponential functions that vanishes to high order at several algebraic
+  points.
+
+## References
+Loo-Keng Hua, Introduction to Number Theory, Springer, 1982. Chapter XII (§13).
+A. O. Gelfond (1934), *Sur le septième Problème de Hilbert
+T. Schneider (1935), *Transzendenzuntersuchungen periodischer Funktionen*
 -/
+
 @[expose] public section
 
-open BigOperators Module.Free Fintype NumberField FiniteDimensional
-  Matrix Set Polynomial Finset IntermediateField Complex
+open BigOperators Module.Free Fintype NumberField Embeddings FiniteDimensional
+   Matrix Set Polynomial Finset IntermediateField Complex AnalyticAt
 
 noncomputable section
 
@@ -85,3 +109,106 @@ variable {K} [Field K] [NumberField K]
 abbrev c' [Field K] [NumberField K] (α : K) : ℤ := (c'_both α : ℤ)
 
 lemma c'_neq0 (α : K) : (c'_both α : ℤ) ≠ 0 := (c'_both α).2.1
+
+
+
+/--
+This structure encapsulates all the foundational data and hypotheses for the proof.
+-/
+structure Setup where
+  (α β : ℂ)
+  (K : Type)
+  [isField : Field K]
+  [isNumberField : NumberField K]
+  (σ : K →+* ℂ)
+  (α' β' γ' : K)
+  hirr : ∀ i j : ℤ, β ≠ i / j
+  htriv : α ≠ 0 ∧ α ≠ 1
+  hα : IsAlgebraic ℚ α
+  hβ : IsAlgebraic ℚ β
+  habc : α = σ α' ∧ β = σ β' ∧ α ^ β = σ γ'
+  hd : DecidableEq (K →+* ℂ)
+
+namespace Setup
+
+attribute [instance] isField isNumberField
+
+variable (h7 : Setup)
+
+open Setup
+
+lemma alpha_cpow_beta_ne_zero : h7.α ^ h7.β ≠ 0 :=
+  fun H => h7.htriv.1 ((cpow_eq_zero_iff h7.α h7.β).mp H).1
+
+lemma beta_ne_zero : h7.β ≠ 0 :=
+  fun H => h7.hirr 0 1 (by simpa [div_one] using H)
+
+lemma alpha'_beta'_gamma'_ne_zero : h7.α' ≠ 0 ∧ h7.β' ≠ 0 ∧ h7.γ' ≠ 0 := by
+  constructor
+  · intro H
+    exact h7.htriv.1 (h7.habc.1 ▸ H ▸ RingHom.map_zero h7.σ)
+  · constructor
+    · intro H; exact h7.beta_ne_zero (h7.habc.2.1 ▸ H ▸ RingHom.map_zero h7.σ)
+    · intro H; exact h7.alpha_cpow_beta_ne_zero (h7.habc.2.2 ▸ H ▸ RingHom.map_zero h7.σ)
+
+lemma alpha'_ne_one : h7.α' ≠ 1 := by
+  intro H
+  apply_fun h7.σ at H
+  rw [← h7.habc.1, map_one] at H
+  exact h7.htriv.2 H
+
+lemma beta'_ne_zero : h7.β' ≠ 0 := h7.alpha'_beta'_gamma'_ne_zero.2.1
+
+open Complex
+
+lemma log_zero_zero : log h7.α ≠ 0 := by
+  intro H
+  have := congr_arg exp H
+  rw [exp_log, exp_zero] at this
+  · apply h7.htriv.2 this
+  · exact h7.htriv.1
+
+def c₁ : ℤ := abs (c' h7.α' * c' h7.β' * c' h7.γ')
+
+lemma one_leq_c₁ : 1 ≤ h7.c₁ := by
+  have h := (mul_ne_zero (mul_ne_zero (c'_neq0 h7.α')
+    (c'_neq0 h7.β')) (c'_neq0 h7.γ'))
+  exact Int.one_le_abs h
+
+lemma c₁_neq_zero : h7.c₁ ≠ 0 :=
+  Ne.symm (Int.ne_of_lt h7.one_leq_c₁)
+
+lemma one_leq_abs_c₁ : 1 ≤ |↑h7.c₁| := by
+  refine Int.one_le_abs (Ne.symm (Int.ne_of_lt h7.one_leq_c₁))
+
+lemma IsIntegral_assoc (K : Type) [Field K]
+{x y : ℤ} (z : ℤ) (α : K) (ha : IsIntegral ℤ (z • α)) :
+  IsIntegral ℤ ((x * y * z : ℤ) • α) := by
+  have : ((x * y * z : ℤ) • α) = (x * y) • (z • α) := by
+    simp only [Int.cast_mul, zsmul_eq_mul, mul_assoc (↑x * ↑y : K) z α]
+  conv => enter [2]; rw [this]
+  apply IsIntegral.smul _ ha
+
+lemma isIntegral_c₁α : IsIntegral ℤ (h7.c₁ • h7.α') := by
+  have h := IsIntegral_assoc (x := c' h7.γ') (y := c' h7.β') h7.K (c' h7.α') h7.α'
+    ((c'_both h7.α').2.2)
+  conv => enter [2]; rw [c₁, mul_comm, mul_comm (c' h7.α') (c' h7.β'), ← mul_assoc]
+  rcases abs_choice (c' h7.γ' * c' h7.β' * c' h7.α')
+  · rename_i H1; rw [H1]; exact h
+  · rename_i H2; rw [H2]; rw [← IsIntegral.neg_iff, neg_smul, neg_neg]; exact h
+
+lemma isIntegral_c₁β : IsIntegral ℤ (h7.c₁ • h7.β') := by
+  have h := IsIntegral_assoc (x := c' h7.γ') (y := c' h7.α') h7.K (c' h7.β') h7.β'
+    ((c'_both h7.β').2.2)
+  rw [c₁, mul_comm, ← mul_assoc]
+  rcases abs_choice (c' h7.γ' * c' h7.α' * c' h7.β')
+  · rename_i H1; rw [H1]; exact h
+  · rename_i H2; rw [H2]; rw [← IsIntegral.neg_iff, neg_smul, neg_neg]; exact h
+
+lemma isIntegral_c₁γ : IsIntegral ℤ (h7.c₁ • h7.γ') := by
+  have h := IsIntegral_assoc (x := c' h7.α') (y := c' h7.β') h7.K (c' h7.γ') h7.γ'
+    ((c'_both h7.γ').2.2)
+  rw [c₁]
+  rcases abs_choice (c' h7.α' * c' h7.β' * c' h7.γ')
+  · rename_i H1; rw [H1]; exact h
+  · rename_i H2; rw [H2]; rw [← IsIntegral.neg_iff, neg_smul, neg_neg]; exact h
